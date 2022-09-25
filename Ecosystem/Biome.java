e789d2f4c5c7499cf44b080b1fc665414e02a41c
DiegoAndresGlez <57697717+DiegoAndresGlez@users.noreply.github.com>
2022-09-24 19:16:17 -0700

    T2.2.1 Relaciones Entre Clases
    Con Generalizacion y Agregacion

---
+++ Ecosystem/Biome.java
@@ -0,0 +1,20 @@
+public class Biome {
+
+	private String biome;
+
+	public String getBiome() {
+		return biome;
+	}
+
+	public void setBiome(String biome) {
+		this.biome = biome;
+	}
+
+	@Override
+	public String toString() {
+		String s = "";
+		s += "Bioma: ";
+		return s;
+	}
+
+}
